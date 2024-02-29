89685e153428cc7a72403fa340a03d28b1fb3a4d
OscarSOX <xosebeloane@gmail.com>
2024-02-29 12:24:02 +0200

    vite -config

--- vite.config.js
+++ vite.config.js
@@ -6,6 +6,7 @@ export default defineConfig({
   build: {
     rollupOptions: {
       // https://rollupjs.org/configuration-options/
+      'build.rollupOptions.external'
     },
   },
   plugins: [react()],
