0f50256a0e675a59cf780a4d8e5a1a3253e307be
OscarSOX <xosebeloane@gmail.com>
2024-02-29 14:26:09 +0200

    single page fix

--- src/pages/Movie/Movie.jsx
+++ src/pages/Movie/Movie.jsx
@@ -1,8 +1,9 @@
 import React, { useEffect, useState } from 'react'
-import { useParams } from 'react-router-dom';
+import { Link, useParams } from 'react-router-dom';
 import { Navbar } from '../../components';
 
 import './Movie.css'
+import { FaArrowLeft } from 'react-icons/fa';
 
 const Movie = () => {
 
@@ -19,32 +20,30 @@ const Movie = () => {
   useEffect(() => {
     getMovie()
   },[])
-
+  
   return (
-    <div className="head | bg-primary w-full h-screen mt-[-100px] xl:max-w-[1280px]">
-        <Navbar />
-        <div className='relative'>
-          <div>
-            <div className="herobanner | absolute left-0 top-0 w-full h-[550px] overflow-hidden">
-              <img className='object-cover object-top' src={`https://image.tmdb.org/t/p/original/${singleMovie?.backdrop_path}`} alt="image" />
-            </div>
-            <div className="herotext">
-              <p className='text-white mb-4'>Movies</p>
-              <h2 className='text-white font-bold text-lg mb-6 leading-normal'>{singleMovie?.title}</h2>
-              <p className='text-white text-base mb-4'>{singleMovie?.overview}</p>
-            </div>
+    <div className='h-dvh'>
+      <Navbar />
+      <div className="bg-primary w-full overflow-hidden">
+        <div className="flex flex-col justify-center items-center sm:px-16 px-8 py-4 relative" style={{ height: '100vh' }}>
+          <div className="heroImage | w-full h-[450px] overflow-hidden -mt-8">
+            <img className='object-cover object-top' src={`https://image.tmdb.org/t/p/original/${singleMovie?.backdrop_path}`} alt="image" />
           </div>
-        </div>
 
-        <div className="bg-primary | flex justify-center items-start sm:px-16 px-6 pb-14 z-[999]">
-        <div className="xl:max-w-[1280px] w-full">
-          <div className="heobanner">
-          <p className='text-white mb-4'>design</p>
-          <h2 className='text-white font-bold text-lg mb-6 leading-normal'>{singleMovie?.title}</h2>
-          <p className='text-white text-base mb-4'>{singleMovie?.overview}</p>
+          <div className="flex justify-center items-center sm:px-16 px-8 py-4 w-full z-10 mt-40">
+            <div className="xl:max-w-[1280px] w-full flex flex-row gap-20">
+              <img className='flex-[1] object-cover object-top w-10' src={`https://image.tmdb.org/t/p/original/${singleMovie?.poster_path}`} alt="image" />
+              <div className="flex flex-col flex-[3]">
+                <h2 className='text-white text-5xl font-bold'>{singleMovie?.title}</h2>
+                <p className='text-base text-white mt-8'>{singleMovie?.overview}</p>
+                <p className='text-base text-gray-500 mt-8'>Releases: {singleMovie?.release_date}</p>
+                <p className='text-base text-orange-500 mt-18'>Rated: <span className='text-white'>{singleMovie?.vote_average}</span></p>
+                <p className='text-base text-orange-500 mt-8 underline'><Link to="/" style={{ display: 'flex', alignItems: 'center', gap: '0.5rem' }}><FaArrowLeft size={15} /> Back</Link></p>
+              </div>
+            </div>
           </div>
         </div>
-      </div>
+    </div>
     </div>
   )
 }
