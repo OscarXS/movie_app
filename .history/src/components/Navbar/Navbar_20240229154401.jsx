1d286da87b3a119d29d1b3fa2e436305c731e6a7
OscarSOX <xosebeloane@gmail.com>
2024-02-29 15:44:57 +0200

    better fix single page

---
+++ .history/src/components/Navbar/Navbar_20240229154401.jsx
@@ -0,0 +1,24 @@
+import React from 'react'
+import { FaHome } from 'react-icons/fa'
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className="flex justify-center items-center sm:px-16 px-8 py-4">
+        <div className="xl:max-w-[1280px] w-full">
+            <div className='header'>
+            
+              <div className="logo | text-orange-500 font-semibold text-2xl"><Link to="">Movies</Link></div>
+              
+              <ul className="menu">
+                  <li className='p-5 rounded-2xl text-white bg-black w-7 h-7 flex justify-center items-center'><Link to="/"><FaHome /></Link></li>
+              </ul>
+            </div>
+          </div>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -8,7 +8,9 @@ const Navbar = () => {
       <div className="flex justify-center items-center sm:px-16 px-8 py-4">
         <div className="xl:max-w-[1280px] w-full">
             <div className='header'>
-              <div className="logo | text-orange-500 font-semibold text-2xl">Movies</div>
+            
+              <div className="logo | text-orange-500 font-semibold text-2xl"><Link to="/">Movies</Link></div>
+              
               <ul className="menu">
                   <li className='p-5 rounded-2xl text-white bg-black w-7 h-7 flex justify-center items-center'><Link to="/"><FaHome /></Link></li>
               </ul>
